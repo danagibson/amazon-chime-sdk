2cab1e142c37e86416499c4a915e84fae6b2a3f2
danagibson <dgibson@san.rr.com>
2023-06-16 21:26:13 -0700

    Update config.js

--- apps/alexa-skill-calling/skill-sample-simple-customer-support/simple-customer-support-skill/lambda/config.js
+++ apps/alexa-skill-calling/skill-sample-simple-customer-support/simple-customer-support-skill/lambda/config.js
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 const skillConfig = {
-  destinationPhoneNumber: 'placeholder',
+  destinationPhoneNumber: '+18582430035',
   sourcePhoneNumber: 'placeholder',
 };
 
